edb3c032144afa2ed13eae7a5c76a5a211263b8d
Lucas Zurverra <lzurverra@gmail.com>
2019-05-04 20:14:43 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+instagram-userfiles-manager makes folders per each user from a set of images/videos downloaded from instagram.
+Files must have following pattern:
+
+`<username>_<sequence_of_numbers>_<sequence_of_numbers>_<sequence_of_numbers>_.[jpg|mp4]`
---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+import shutil
+import re
+
+quantity = 0
+regex = "(.*)_\d+_\d+_\d+_n.[jpg|mp4]"
+
+for root, dirs, files in os.walk("/Users/lucas/Desktop/instagram"):
+    for i, entry in enumerate(files):
+        match = re.search(regex, entry)
+        # Delete Mac OS X attributes file
+        os.system("rm {0}/.DS_Store".format(root))
+        user = match.group(1)
+        dir_from = root + "/{}".format(entry)
+        dir_to = root + "/{}".format(user)
+        try:
+            os.makedirs(dir_to)
+        except OSError:
+            pass
+        print "Moving: {0} {1}".format(match, dir_to)
+        os.system("mv {0} {1}".format(dir_from, dir_to))
+        quantity += 1
+raw_input("""
+{0} files moved succesfully.
+Press any key to contiune...
+""".format(quantity))
