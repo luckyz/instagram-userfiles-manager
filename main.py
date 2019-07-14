da268abba33e250ba4221dd60ae1c1f5b0cf31a4
Lucas Zurverra <lzurverra@gmail.com>
2019-07-13 22:56:45 -0300

    errors handled

--- main.py
+++ main.py
@@ -5,26 +5,33 @@ import re
 quantity = 0
 regex = "(.*)_\d+_\d+_\d+_n.[jpg|mp4]"
 
+errors = []
+
 if os.path.exists("/Users/lucas/Desktop/instagram/.DS_Store"):
     os.remove("/Users/lucas/Desktop/instagram/.DS_Store")
 
-for root, dirs, files in os.walk("/Users/lucas/Desktop/instagram"):
-    for i, entry in enumerate(files):
-        match = re.search(regex, entry)
-        # Delete Mac OS X attributes file
-        os.system("rm {0}/.DS_Store".format(root))
-        print entry, match
-        user = match.group(1)
-        dir_from = root + "/{}".format(entry)
-        dir_to = root + "/{}".format(user)
-        try:
-            os.makedirs(dir_to)
-        except OSError:
-            pass
-        print "Moving: {0} {1}".format(match, dir_to)
-        os.system("mv {0} {1}".format(dir_from, dir_to))
-        quantity += 1
-raw_input("""
-{0} files moved succesfully.
-Press any key to contiune...
-""".format(quantity))
+try:
+    for root, dirs, files in os.walk("/Users/lucas/Desktop/instagram"):
+        for i, entry in enumerate(files):
+            match = re.search(regex, entry)
+            user = match.group(1)
+            dir_from = root + "/{}".format(entry)
+            dir_to = root + "/{}".format(user)
+            if not user in dirs:
+                os.makedirs(dir_to)
+            if entry in dirs:
+                print entry, dirs
+            os.system("mv {0} {1}".format(dir_from, dir_to))
+            quantity += 1
+except Exception as e:
+    errors.append(e)
+    pass
+except FileExists:
+    pass
+
+print """{0} files moved succesfully.""".format(quantity)
+if len(errors) > 0:
+    print "\nErrors:"
+    for i, error in enumerate(errors):
+        print "[{0}] - {1}".format(i, error)
+raw_input("""Press [ENTER] to continue...""")
