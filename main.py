1076c1ad61f8a48e53b249745fcd83181ee63b9f
Lucas Zurverra <lzurverra@gmail.com>
2019-12-24 18:12:11 -0300

    added commandline args support

--- main.py
+++ main.py
@@ -1,30 +1,56 @@
 import os
 import shutil
 import re
+from sys import argv
 
 quantity = 0
-regex = "(.*)_\d+_\d+_\d+_n.[jpg|mp4]"
+regex = "(.*)_\d+_\d+_\d+_n.(jpg|png|mp4)"
+
+# Picture username recognition
+#flag = None
+#count = 0
 
 errors = []
 
 try:
-    workdir = raw_input("> Please, enter the working directory: ")
+    # Get commandline arguments
+    if argv[1] != None:
+        workdir = argv[1]
+    else:
+        workdir = raw_input("> Please, enter the working directory: ")
 
+    # Deletes MacOs file
     if os.path.exists("{}/.DS_Store".format(workdir)):
         os.remove("{}/.DS_Store".format(workdir))
 
+    # Explores subdirectories
     for root, dirs, files in os.walk(workdir):
         for i, entry in enumerate(files):
             match = re.search(regex, entry)
+            # Detects username
             user = match.group(1)
             dir_from = root + "/{}".format(entry)
             dir_to = root + "/{}".format(user)
-            if not user in dirs:
-                os.makedirs(dir_to)
-            if entry in dirs:
+
+            # Moves picture to user dir
+            if user in dirs:
                 print entry, dirs
+            # Creates dir if not exist
+            else:
+                os.makedirs(dir_to)
             os.system("mv {0} {1}".format(dir_from, dir_to))
             quantity += 1
+
+            # # Picture username recognition
+            # while flag not in ("yes", "y", "no", "n"):
+            #     count =+ 1
+            #     if count == 1:
+            #         flag = raw_input("> Use picture username recognition? (default: [n]): ")
+            #     else:
+            #         flag = raw_input("> Sorry, you should answer 'yes' [y] or 'no [n]. Use picture username recognition? (default: [n]): ")
+            # if flag in ("yes", "y"):
+            #     os.system("python3.7 image2text/main.py -i {0}/{1} -o {0}".format(workdir, entry))
+
 except Exception as e:
     errors.append(e)
     pass
@@ -36,4 +62,4 @@ if len(errors) > 0:
     print "\nErrors:"
     for i, error in enumerate(errors):
         print "[{0}] - {1}".format(i, error)
-raw_input("""Press [ENTER] to continue...""")
+raw_input("""\nPress [ENTER] to continue...""")
