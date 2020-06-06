85fb3a2d4cc1934005db4595d211cee85e88713d
Lucas Zurverra <lzurverra@gmail.com>
2020-06-06 20:36:16 -0300

    deleted unused package

--- image2text/constants.py
+++
@@ -1,5 +0,0 @@
-DEFAULT_CHECK_COMMAND = "which"
-WINDOWS_CHECK_COMMAND = "where"
-TESSERACT_DATA_PATH_VAR = 'TESSDATA_PREFIX'
-
-VALID_IMAGE_EXTENSIONS = [".jpg", ".jpeg", ".gif", ".png", ".tga", ".tif", ".bmp"]
